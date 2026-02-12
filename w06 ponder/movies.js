be096a8340b889e75c475fdf12084304d2336eae
rya20002 <rya20002@byui.edu>
2026-02-12 14:22:45 -0700

    w06 ponder fix

--- w06 ponder/movies.js
+++ w06 ponder/movies.js
@@ -48,15 +48,15 @@ const movies = [
         
        let html = `
             <h2>${movie.title}</h2>
-            <img src=${movie.imgSrc} alt=${movie.imgAlt}>
+            <img src="${movie.imgSrc}" alt="${movie.imgAlt}">
             <p><strong>Release Date:</strong>${movie.date}</p>
-            <p><strong>Recommended Age:</strong>${movie.ages}>
+            <p><strong>Recommended Age:</strong>${movie.ages}></p>
             <p><strong>Genre:</strong>${movie.genre}</p>
-            <p><strong>Rating:</strong> <span aria-label=${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
+            <p><strong>Rating:</strong> <span aria-label="${movie.stars.length} out of 5 stars" role="img">${movie.stars}</span></p>
             <p id='desc'>${movie.description}</p>
         `
 
-        article.HTML = html;
+        article.innerHTML = html;
         movieList.appendChild(article);
     });
           
