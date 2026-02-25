29864b2876ffd18585969a048d1b6c4bf20ca07a
rya20002 <rya20002@byui.edu>
2026-02-25 15:51:56 -0700

    fix 3

--- w08 ponder/courses.js
+++ w08 ponder/courses.js
@@ -1,7 +1,7 @@
 const aCourse = {
     code: 'CSE121b',
     name: 'Javascript Language',
-    logo: 'images/js-logo.png',
+    logo: 'js-logo.png',
     sections: [
     { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
     { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
